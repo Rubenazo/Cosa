07d9f9efd0dd64b0a439ddf08dd55c3c82783ff9
Rubenazo <rubenx_360@hotmail.com>
2015-03-24 13:53:00 -0430

    BD actualizada

--- cosa.sql
+++ cosa.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-03-2015 a las 03:29:52
+-- Tiempo de generación: 24-03-2015 a las 19:20:45
 -- Versión del servidor: 5.6.11
 -- Versión de PHP: 5.5.3
 
@@ -59,7 +59,18 @@ CREATE TABLE IF NOT EXISTS `num_cuenta` (
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`),
   KEY `banco_id` (`banco_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=6 ;
+
+--
+-- Volcado de datos para la tabla `num_cuenta`
+--
+
+INSERT INTO `num_cuenta` (`id`, `banco_id`, `numero`, `tipo`, `created_at`, `updated_at`) VALUES
+(1, 1, 12345, 'corriente', '2015-03-17 21:57:20', '0000-00-00 00:00:00'),
+(2, 2, 56789, 'ahorro', '2015-03-17 21:57:20', '0000-00-00 00:00:00'),
+(3, 1, 45533, 'nolmal', '2015-03-18 18:52:23', '0000-00-00 00:00:00'),
+(4, 2, 74488, 'anolmal', '2015-03-18 18:52:23', '0000-00-00 00:00:00'),
+(5, 1, 45452, 'perro', '2015-03-18 23:54:32', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -109,6 +120,29 @@ INSERT INTO `planes` (`id`, `descripcion`, `precio`, `velocidad`, `created_at`, 
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `remember_token` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'pedro', '$2y$10$cglR882hKdIqgQC8SN5TBuUPFdzKKgkyvbNdRQStZT4K4bXyyAmeG', 'y8s5Xk9PXRYZKxQbCHEV5IXoQ6fudvgGcFyeOTNYyC5e1Da3HkUPnRldZuzK', '2015-03-24 18:18:20', '2015-03-24 22:48:20');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
@@ -123,7 +157,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`),
   KEY `plan_id` (`plan_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=6 ;
 
 --
 -- Volcado de datos para la tabla `usuarios`
@@ -133,7 +167,8 @@ INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `correo`, `zona`, `plan_id`,
 (1, 'Ruben', 'Gonzalez', 'rubenx_360@hotmail.com', 'maracay', 1, '2015-03-08 03:28:00', '2015-03-08 03:28:00'),
 (2, 'Pedro', 'Perez', 'Pepin@hotmail.com', 'jueves', 1, '2015-03-08 03:29:56', '2015-03-08 03:29:56'),
 (3, 'Jaja', 'Jeje', 'jfjf@fjjf.com', 'rrt', 1, '2015-03-08 03:34:11', '2015-03-08 03:34:11'),
-(4, 'chikun', 'guya', 'zan@hotmail.com', 'aqui', 2, '2015-03-11 05:04:18', '2015-03-11 05:04:18');
+(4, 'chikun', 'guya', 'zan@hotmail.com', 'aqui', 2, '2015-03-11 05:04:18', '2015-03-11 05:04:18'),
+(5, 'ds', 'dsds', 'dsdsd@dd.com', 'sdsd', 1, '2015-03-13 17:49:40', '2015-03-13 17:49:40');
 
 --
 -- Restricciones para tablas volcadas
@@ -149,10 +184,10 @@ ALTER TABLE `num_cuenta`
 -- Filtros para la tabla `pagos`
 --
 ALTER TABLE `pagos`
-  ADD CONSTRAINT `pagos_ibfk_4` FOREIGN KEY (`num_cuenta_id`) REFERENCES `num_cuenta` (`id`),
   ADD CONSTRAINT `pagos_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`),
   ADD CONSTRAINT `pagos_ibfk_2` FOREIGN KEY (`plan_id`) REFERENCES `planes` (`id`),
-  ADD CONSTRAINT `pagos_ibfk_3` FOREIGN KEY (`banco_id`) REFERENCES `bancos` (`id`);
+  ADD CONSTRAINT `pagos_ibfk_3` FOREIGN KEY (`banco_id`) REFERENCES `bancos` (`id`),
+  ADD CONSTRAINT `pagos_ibfk_4` FOREIGN KEY (`num_cuenta_id`) REFERENCES `num_cuenta` (`id`);
 
 --
 -- Filtros para la tabla `usuarios`
